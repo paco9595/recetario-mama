80b8d52abb73644b96d5c0045e60f2b184d0dc63
Francisco VIllarreal <paco_vip95@hotmail.com>
2025-03-29 17:05:25 -0600

    connect db

--- backend/src/sqlConnection.ts
+++ backend/src/sqlConnection.ts
@@ -1,16 +1,3 @@
-import postgres from 'postgres'
+const { neon } = require("@neondatabase/serverless");
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-
-
-export const sql = postgres({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-  ssl: 'require',
-  connection: {
-    options: `project=${ENDPOINT_ID}`,
-  },
-});
+export const sql = neon(process.env.DATABASE_URL);
