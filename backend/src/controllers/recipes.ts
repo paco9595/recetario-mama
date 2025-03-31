0ccfe4808739c015b63057c759f63fc6224eba40
Francisco VIllarreal <paco_vip95@hotmail.com>
2025-03-31 15:00:53 -0600

    update recipes controllers

--- backend/src/controllers/recipes.ts
+++ backend/src/controllers/recipes.ts
@@ -1,23 +1,29 @@
 import { Request, Response } from "express";
-import { dataRecipes } from "../data/recipesData";
 import { sql } from "../sqlConnection";
 
 export async function getAllRecipes(req: Request, res: Response) {
-  const dataRecipes = await sql`select * from recipes`
-  res.send({data:dataRecipes})
+  const { userId } = req.params
+  const dataRecipes = await sql`select id, title, 'description', tag, image_url from recipes where user_id = ${userId} `
+  res.send({ data: dataRecipes })
 }
 
 export async function getRecipe(req: Request, res: Response) {
-  const id = req.params.id;
+  const {id, userId}= req.params;
   console.log(id);
-  const recipe = await sql`select * from recipes where id = ${id}`;
-  res.send({data: recipe, error: undefined});
+  const recipe = await sql`select * from recipes where id = ${id} and user_id = ${userId}`;
+  res.send({ data: recipe, error: undefined });
 }
 
 
-export function createNewRecipe(req: Request, res: Response) {
-  const data = req.body;
-  console.log(JSON.stringify(data))
-  res.send({ id: 'test', error: undefined })
+export async function createNewRecipe(req: Request, res: Response) {
+  const {title,description, steps, ingredients} = req.body;
+  const returnData = await sql`INSERT INTO recipes (title, description, steps, ingredients)
+  VALUES (
+      ${title},
+      ${description},
+      ${steps},
+      ${ingredients}
+  );`
+  console.log(returnData)
 }
 
