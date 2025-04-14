bb1631bb54215b62647897b079478b762845e3dd
Francisco VIllarreal <paco_vip95@hotmail.com>
2025-04-14 13:23:42 -0600

    validate for the correct event name

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -4,7 +4,7 @@ import { createUserService } from "../services/authService";
 export async function callback(req: Request, res: Response, next: NextFunction) {
   const event = req.body;
   console.log(req.body)
-  if (event.type === 'user.created') {
+  if (event.type === 'email.created') {
     const { id, email_addresses, first_name, last_name } = event.data;
     const email = email_addresses?.[0]?.email_address || null;
 
